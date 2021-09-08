1cf507308e8e24506bb1aee9c7857c788fe02843
Victor Hugo <vhps800@gmail.com>
2021-09-08 18:16:09 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import Barchart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -6,16 +8,31 @@ import NavBar from "components/NavBar";
 function App() {
   return (
     <>
-    <NavBar/>
-    <div className = "container"> 
-    <h1 className="text-primary">Olá Mundo!!</h1>
-    
-    <DataTable />
-    </div>
-    <Footer />
- 
-    </> //Fragmentação = permite que varios elementos sejam retornados na mesma função
-  );
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas!!</h1>
+
+        <div className="row px-3" >
+          <div className="col-sm-6">
+            <h5 className ="text center text-secondary"> Taxa de Sucesso</h5>
+            <Barchart />
+            </div>
+              <div className="col-sm-6">
+                <h5> Taxa de Sucesso</h5>
+                <DonutChart />
+            </div>
+            </div>
+
+            <div className = "py-3">
+             <h2 className = "text-primary"> Todas vendas</h2>
+            </div>
+
+
+          <DataTable />
+        </div>
+        <Footer />
+      </>  //Fragmentação = permite que varios elementos sejam retornados na mesma função
+      );
 }
 
 export default App;
